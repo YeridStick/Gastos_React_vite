bca8d03bb69cb3c075bf976df9cc984e0c72c5cd
Yerid Stick <yeridstick3000@gmail.com>
2025-04-09 17:31:28 -0500

    fix: boton flotaten - para vista movil

--- src/App.jsx
+++ src/App.jsx
@@ -478,30 +478,7 @@ function App() {
                 />
               )}
             </div>
-          </main>
-
-          {/* Botón flotante para móviles */}
-          <div className="md:hidden">
-            <button
-              onClick={handleNuevoGasto}
-              className="fixed bottom-8 right-8 bg-blue-600 text-white p-4 rounded-full shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                ></path>
-              </svg>
-            </button>
-          </div>
+          </main>          
         </div>
       ) : (
         // Componente de inicio para establecer presupuesto
